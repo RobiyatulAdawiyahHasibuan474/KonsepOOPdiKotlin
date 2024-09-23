07ce127ecbefe12ac5e294289b8c90bd469eaa53
ACER <robiyatuladawiyahh479@gmail.com>
2024-09-23 21:02:06 +0700

    Add InheritanceA.kt

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/InheritanceA.kt
@@ -0,0 +1,15 @@
+package id.ac.polbeng.depandi.test_kelas
+
+open class Computer(val name: String, val brand: String) {
+}
+class Laptop(name: String, brand: String, val batteryLife: Double) : Computer(name, brand) {
+    fun info(){
+        println("Name : $name")
+        println("Brand : $brand")
+        println("Battery Life : $batteryLife")
+    }
+}
+fun main(){
+    val myLaptop = Laptop("Acer Aspire 4738", "Acer", 2.5)
+    println(myLaptop.info())
+}
